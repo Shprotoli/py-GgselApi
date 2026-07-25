e4402b2320ff3a1ec110412d762129a6d73bffed
= <ggrerqq2346@rambler.ru>
2026-07-25 21:54:04 +0300

    add [other]: Add new import object in `__init__`

--- ggsel_py/__init__.py
+++ ggsel_py/__init__.py
@@ -1,2 +1,2 @@
-from ggsel_py.api.ggsel_api import GgselApiV1, GgselApiV2
+from ggsel_py.api.ggsel_api import GgselApiMaster, GgselApiV1, GgselApiV2
 from ggsel_py.api.client import SyncGClient, AsyncGClient
